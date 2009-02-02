11862030c6d758c0ffb00cde6fae8e2558611ca7
Jay Williams <jay@myd3.com>
2009-02-02 16:07:51 -0600

    Added SQLite test.

---
+++ test1.php
@@ -0,0 +1,14 @@
+<?php
+
+
+echo "<pre>\nDebug Information:\n";
+
+// version 
+echo "SQLite version: ".sqlite_libversion()."\n"; 
+// encoding 
+echo "SQLite encoding: ".sqlite_libencoding()."\n";
+
+echo "</pre>\n" 
+
+
+?>

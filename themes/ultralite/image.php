157edff4ff6c320c5040abc796feb4dd44c2089c
David Kozikowski <david.kozikowski@gmail.com>
2009-02-03 14:12:54 -0500

    A little CSS never hurt anyone... Yeah, I'm bored!

--- themes/ultralite/image.php
+++ themes/ultralite/image.php
@@ -4,6 +4,55 @@
 	<meta http-equiv="Content-type" content="text/html; charset=utf-8" />
 	
 	<title><?php echo $site_title; ?></title>
+	
+	<style type="text/css">
+		* {
+			margin: 0;
+			padding: 0;
+		}
+
+		body {
+			width: <?php echo $image_width; ?>px;
+			margin: 0 auto;
+			text-align: center;
+			font: 14px "Lucida Grande", Lucida, Verdana, sans-serif;
+		}
+
+		#ultralite {
+			text-align: left;
+		}
+
+		h1 {
+			font-size: 2.0em;
+		}
+
+		h2 {
+			font-size: 1.3em;
+		}
+
+		h3#title {
+			font-size: 1.6em;
+			float: left;
+		}
+
+		h2,h3 {
+			margin-bottom: 12px;
+		}
+		
+		#published {
+			float: right;
+			margin-top: 10px;
+		}
+
+		img#photo {
+			border: 1px solid #000;
+		}
+
+		#description p {
+			padding: 12px;
+			text-align: justify;
+		}
+	</style>
 </head>
 
 <body>
@@ -13,7 +62,9 @@
 	
 	<h2><?php echo $site_slogan; ?></h2>
 	
-	<h3><?php echo $image_title; ?></h3>
+	<h3 id="title"><?php echo $image_title; ?></h3>
+	
+	<span id="published"><?php echo $image_published; ?></span>
 	
 	<img src="images/<?php echo $image_filename; ?>" alt="<?php echo $image_title; ?>" <?php echo $image_dimensions; ?> id="photo" />
 	

5a6d1879704557474786c4fe7160c9b651a34208
David Kozikowski <david.kozikowski@gmail.com>
2009-02-02 23:49:43 -0500

    Output test using foreach()

--- db_test.php
+++ db_test.php
@@ -9,7 +9,7 @@ $db = new ezSQL_pdo();
 
 
 /*
-	Make sure the file is writable, otherwise php won't will error out, 
+	Make sure the file is writable, otherwise php will error out, 
 	and won't be able to add anyting to the database.
 */
 $db->connect('sqlite:./pixelpost.sqlite3');
@@ -19,7 +19,7 @@ $db->connect('sqlite:./pixelpost.sqlite3');
 	CREATE  TABLE  IF NOT EXISTS "main"."pixelpost" ("id" INTEGER PRIMARY KEY  NOT NULL , "title" VARCHAR NOT NULL  DEFAULT 'Untitled', "description" TEXT NOT NULL , "filename" VARCHAR NOT NULL , "published" DATETIME NOT NULL  DEFAULT CURRENT_TIMESTAMP)
 */
 
-$db->query("CREATE  TABLE  IF NOT EXISTS 'main'.'pixelpost' ('id' INTEGER PRIMARY KEY  NOT NULL , 'title' VARCHAR NOT NULL  DEFAULT 'Untitled', 'description' TEXT NOT NULL , 'filename' VARCHAR NOT NULL , 'published' DATETIME NOT NULL  DEFAULT CURRENT_TIMESTAMP)");
+$db->query("CREATE TABLE IF NOT EXISTS 'main'.'pixelpost' ('id' INTEGER PRIMARY KEY NOT NULL, 'title' VARCHAR NOT NULL DEFAULT 'Untitled', 'description' TEXT NOT NULL, 'filename' VARCHAR NOT NULL, 'published' DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP)");
 
 /*
 	INSERT INTO "main"."pixelpost" ("title","description","filename","published") VALUES ('MyTitle','MyDescription','myimage.jpg',CURRENT_TIMESTAMP)
@@ -31,5 +31,13 @@ $rows = $db->get_results("SELECT * FROM pixelpost");
 
 var_dump($rows);
 
+/*
+	Output test using foreach().
+*/
+foreach($rows as $row)
+{
+	var_dump($row->id);
+	var_dump($row->title);
+}
 
 ?>

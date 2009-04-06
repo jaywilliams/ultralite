8a7d942fd444104b82377db8ae736904b1d9097d
Jay Williams <jay@myd3.com>
2009-04-06 16:33:43 -0500

    Example language file

---
+++ languages/language_de.php
@@ -0,0 +1,20 @@
+<?php
+
+# Language Information:
+$language['_name']       = 'German';
+$language['_country']    = 'Germany';
+$language['_direction']  = 'ltr';
+$language['_translator'] = 'Team Pixelpost <thecrew@pixelpost.prg>';
+
+
+#: index.php:66
+$language['my language variable!'] = 'Meine Sprache Variable!';
+
+#: includes/functions.php:22
+#: plugins/plugin_example.php:8
+$language['Another time, another language'] = 'Ein anderes Mal, eine andere Sprache';
+
+
+
+
+?>

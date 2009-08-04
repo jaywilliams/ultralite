a1cbcb5ebb0556efca9150951de13f836e0d20e3
Jay Williams <jay@d3done.com>
2009-08-04 13:18:28 -0500

    feed_icon.png is now included in RSS feed, if it exists.

--- application/modules/feed/controllers/feedcontroller.php
+++ application/modules/feed/controllers/feedcontroller.php
@@ -98,6 +98,14 @@ class feedController extends baseController implements IController
 			);
 		
 		/**
+		 * Include the feed icon, if it exists:
+		 */
+		if (file_exists(__THEME_PATH."/{$this->_template}/images/feed_icon.png"))
+		{
+			$this->feed['rss']['channel']['atom:icon']   = "{$this->_config->url}content/themes/{$this->_template}/images/feed_icon.png";
+		}
+		
+		/**
 		 * Feed Items
 		 */
 		$this->feed['rss']['channel']['item'] = array();
